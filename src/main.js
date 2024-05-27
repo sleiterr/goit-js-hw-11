cdc2ef528688988b0dcd574a27ecde44a0012444
Oleg Troian <sleite4r@gmail.com>
2024-05-27 20:43:02 +0200

    update commit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,36 @@
+//main.js
+import { fetchImages } from './js/pixabay-api.js';
+import { renderGallery, showNotification } from './js/render-functions.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 document
   .getElementById('searchForm')
-  .addEventListener('submit', function (event) {
-    event.preventDefault(); // Зупиняє стандартне відправлення форми
+  .addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-    const searchInput = document.getElementById('searchInput');
-    const searchQuery = searchInput.value.trim();
-
-    if (searchQuery === '') {
-      alert('Please enter a search query.');
-      return; // Зупиняє подальше виконання, якщо поле порожнє
+    const query = event.target.elements.search.value.trim();
+    if (!query) {
+      showNotification('Please enter a search term');
+      return;
     }
-    
+
+    document.getElementById('results').innerHTML = '<div class="loader">Loading...</div>';
+
+    try {
+      const data = await fetchImages(query);
+
+      if (data.hits.length === 0) {
+        showNotification('Sorry, there are no images matching your search query. Please try again!');
+      } else {
+        renderGallery(data.hits);
+      }
+    } catch (error) {
+      showNotification(error.message);
+    } finally {
+      const loader = document.querySelector('.loader');
+      if (loader) {
+        loader.remove();
+      }
+    }
   });
